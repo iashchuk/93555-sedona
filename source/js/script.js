532f559eeabdaab736c513b6d726ff3023b1e5a1
iashchuk <221box@list.ru>
2018-06-23 22:33:29 +0300

    Подготовка проекта к защите. Формирование папки build без исходников

--- source/js/script.js
+++
@@ -1,6 +0,0 @@
-var button = document.querySelector(".search__link");
-var form = document.querySelector(".hotel-search");
-button.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    form.classList.toggle("hotel-search__show");
-});
